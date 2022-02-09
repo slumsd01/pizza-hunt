43bcb7451b36223f657efa0f61e1a9cb25f6c6ae
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-02-09 01:00:32 -0500

    create Pizza controllers

---
+++ models/Pizza.js
@@ -0,0 +1,20 @@
+const {Schema, model} = require('mongoose')
+
+const PizzaSchema = new Schema ({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+})
+
+const Pizza = model('Pizza', PizzaSchema)
+
+module.exports = Pizza;
